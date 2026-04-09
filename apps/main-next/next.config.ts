2f39f04b12a40123b7e58c1b7ab7ac62964fba9c
panzhanfei <13679383435@163.com>
2026-04-10 03:21:16 +0800

    增加 next  output: "standalone"

--- apps/main-next/next.config.ts
+++ apps/main-next/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false, // 临时关闭测试
+  output: "standalone",
 };
 
 export default nextConfig;
